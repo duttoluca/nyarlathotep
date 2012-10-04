e6a0ee24662703ef04d2258c7ca67a44755b264a
Luca Dutto <duttoluca@gmail.com>
2012-10-04 16:55:09 +0200

    urls.py

--- nyarlathotep/urls.py
+++ nyarlathotep/urls.py
@@ -14,10 +14,11 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', ListView.as_view(model=Post,
                                 queryset=Post.objects.get_visible(),
-                                paginate_by=5), name='home'),
+                                paginate_by=5),
+        name='home'),
     url(r'^post/(?P<slug>[-\w]+)$', DetailView.as_view(model=Post,
                                                        queryset=Post.objects.get_visible()),
-                                                       name='post'),
+        name='post'),
     # url(r'^nyarlathotep/', include('nyarlathotep.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
