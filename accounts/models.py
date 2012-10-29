d28e3d46cd9b1005f324de80452d3110c13d6140
Luca Dutto <duttoluca@gmail.com>
2012-10-29 13:13:26 +0100

    Aggiunto signal per creazione UserProfile in inserimento User

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
     #other user fields
-    URL = models.URLField(help_text="Sito web", blank=True)
-    birth = models.DateField(help_text="Data di nascita", blank=True, verbose_name="Data di nascita")
-    bio = models.TextField(help_text="Qualcosa su di te...", blank=True, verbose_name="Bio")
+    URL = models.URLField(help_text="Sito web", blank=True, null=True,)
+    birth = models.DateField(help_text="Data di nascita", blank=True, null=True, verbose_name="Data di nascita")
+    bio = models.TextField(help_text="Qualcosa su di te...", blank=True, null=True, verbose_name="Bio")
+
+
+# signals
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
